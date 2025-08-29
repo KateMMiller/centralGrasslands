2bda4989dc3322dda831f0e28a93541f340d4ba4
KateMMiller <kathryn_miller@nps.gov>
2025-08-29 12:03:18 -0400

    code update and merging

--- R/prep_data_for_shiny.R
+++ R/prep_data_for_shiny.R
@@ -4,81 +4,134 @@
 library(raster)
 library(sf)
 library(tidyverse)
-library(spatialEco)
+#library(spatialEco)
+library(terra)
 
 sf_use_s2(FALSE)
 
-# #-- Converting to WGS84, so works with leaflet. Only have to run once, so commenting out after complete.
-nps_im <- st_read("./data/GIS/CGI_parks_network.shp")
-nps_im_1km <- st_buffer(nps_im, 1000)
-nps_im_10km <- st_buffer(nps_im, 10000)
-
-nps_im_wgs <- st_transform(nps_im, 4326)
-st_crs(nps_im_wgs)
-nps_im_wgs_cent <- st_centroid(nps_im_wgs)
-nps_cent_latlong <- data.frame(st_drop_geometry(nps_im_wgs_cent),
-                               long = st_coordinates(nps_im_wgs_cent)[,1],
-                               lat = st_coordinates(nps_im_wgs_cent)[,2]
+#-- Generated the shapefile below from NPS Administrative Boundaries,
+# and selected only parks in CGI, then converted to WGS84 in ArcGIS Pro
+nps_im <- st_read("./data_final/GIS/NPS_units_in_CGI_20250827_WGS84_pre.shp")
+nps_im_cent <- st_centroid(nps_im)
+nps_cent_latlong <- data.frame(st_drop_geometry(nps_im_cent),
+                               long = st_coordinates(nps_im_cent)[,1],
+                               lat = st_coordinates(nps_im_cent)[,2]
                                )
+networks <- st_read("./data_final/GIS/networks_wgs84.shp")
 
-nps_im_wgs2 <- left_join(nps_im_wgs, nps_cent_latlong[,c("UNIT_CODE", "long", "lat")],
-                       by = c("UNIT_CODE"))
+# Add centroid to attribute table
+nps_im2 <- left_join(nps_im, nps_cent_latlong[,c("UNIT_CODE", "long", "lat")],
+                     by = c("UNIT_CODE"))
 
-st_write(nps_im_wgs2, "./data/GIS/CGI_parks_network_wgs84.shp", append = FALSE)
+# Add network info via csv vetted by IMD protocol leads
+imd_info <- read.csv("./data_final/IMD_networks_grassland_monitoring_info_20250827.csv")
 
-nps_im_1km_wgs <- st_transform(nps_im_1km, 4326)
-#st_crs(nps_im_1km_wgs)
-st_write(nps_im_1km_wgs, "./data/GIS/CGI_parks_network_1km_wgs.shp", append = FALSE)
+nps_im3 <- left_join(nps_im2, imd_info, by = c("UNIT_CODE" = "Park"))
+nps_im_val <- st_make_valid(nps_im3) |> select(-GIS_Notes, -METADATA, -GlobalID, -Editor,
+                                               -Shape_Area, -long.x, -lat.x) |>
+  rename(long = long.y, lat = lat.y)
+
+head(nps_im_val)
+
+st_write(nps_im_val, "./data_final/GIS/NPS_units_in_CGI_20250827_WGS84.shp", append = FALSE)
+nps_im_val <- st_read("./data_final/GIS/NPS_units_in_CGI_20250827_WGS84.shp")
+
+#create buffers
+sf_use_s2(TRUE) # so dist is in m
+nps_im_1km <- st_buffer(nps_im_val, dist = 1000)
+nps_im_10km <- st_buffer(nps_im_val, dist = 10000)
+
+st_write(nps_im_1km, "./data_final/GIS/NPS_units_in_CGI_20250827_WGS84_1km.shp", append = FALSE)
+st_write(nps_im_10km, "./data_final/GIS/NPS_units_in_CGI_20250827_WGS84_10km.shp", append = FALSE)
+
+cgr_border <- st_read("./data_final/GIS/Grasslands_Roadmap_boundary_Aug_2021_WGS84.shp")
+
+# Commenting out after first run
+# Clipping CGR raster the 10km buffer of parks
+# cgr_ras <- raster::raster("./data_final/GIS/CGR_GAM_V2_WGS84.tif")
+# cgr_10km <- mask(cgr_ras, nps_im_10km)
+# writeRaster(cgr_10km, "./data_final/GIS/CGR_GAM_V2_WGS84_10km.tif")
+
+# Converting CGR 10km raster to polygons via terra package
+# nps_im_terra <- terra::vect("./data_final/GIS/NPS_units_in_CGI_20250827_wgs84.shp")
+# cgr_ras <- terra::rast("./data_final/GIS/CGR_GAM_V2_WGS84_10km.tif")
+# cgr_ras_wgs <- terra::project(cgr_ras, "EPSG:4326")
+# writeRaster(cgr_ras_wgs, "./data_final/GIS/CGR_GAM_V2_WGS84_10km.tif", overwrite = T)
 #
-nps_im_10km_wgs <- st_transform(nps_im_10km, 4326)
-st_crs(nps_im_10km_wgs)
-st_write(nps_im_10km_wgs, "./data/GIS/CGI_parks_network_10km_wgs.shp", append = FALSE)
+# cgr_shp <- terra::as.polygons(cgr_ras_wgs, round = T, aggregate = T, values = T, na.rm = T)
+# writeVector(cgr_shp, "./data_final/CGR_GAM_V2_WGS84_10km.shp", overwrite = T)
+# cgr_shp_simp <- terra::simplifyGeom(cgr_shp, tolerance = 0.001, preserveTopology = TRUE, makeValid = T)
+# writeVector(cgr_shp_simp, "./data_final/CGR_GAM_V2_WGS84_10km_simp.shp", overwrite = T)
 
-# Network boundaries
-nets <- st_read('./data/GIS/networks.shp') |> st_transform(4326)
-names(nets)[names(nets) == "NAME2_"] <- "NETNAME"
-st_write(nets, "./shiny/data/networks_wgs.shp", append = F)
+cgr_shp <- terra::vect("./data_final/GIS/CGR_GAM_V2_WGS84_10km.shp")
 
-# cgr <- terra::rast("./data/GIS/CGR_GAM_V2.tif")
-# cgr_wgs <- terra::project(cgr, crs(nps_im_wgs), threads = 20)
-# terra::writeRaster(cgr_wgs, "./data/GIS/CGR_GAM_V2_WGS84.tif")
+# Clipping CGR raster to boundary of parks
+cgr_ras_orig <- terra::rast("./data_final/GIS/CGR_GAM_V2_WGS84.tif")
+cgr_ras_orig_wgs <- terra::project(cgr_ras_orig, "EPSG:4326")
+cgr_nps <- terra::mask(cgr_ras_orig_wgs, nps_im_val)
+terra::writeRaster(cgr_nps, "./data_final/GIS/CGR_GAM_V2_WGS84_parks.tif", overwrite = T)
 
-# tried to make a map tile- didn't work
-# library(raster)
-# tile(file = "./data/GIS/CGR_GAM_V2_WGS84.tif",
-#      tiles = "./data/GIS/",
-#      zoom = "0:10",
-#      crs = 4326)
-#
-# cgr_ras <- terra::rast("./data/GIS/CGR_GAM_V2_park10km_extract.tif")
-# cgr_wgs <- terra::project(cgr_ras, crs(nps_im_wgs), threads = 20)
-# terra::writeRaster(cgr_wgs, "./data/GIS/CGR_GAM_V2_WGS84_10km.tif")
+#-- Analyze habitat types by park ----
+# Switching to raster package because it worked better
+cgr_ext_park <- raster::raster("./data_final/GIS/CGR_GAM_V2_WGS84_parks.tif")
+dat <- levels(cgr_ext_park)[[1]] #++++ RUNNING HERE ++++
+dat$Category <- c("No Data", "Core Grassland", "Desert/Shrub",
+                  "Vulnerable Grasslands",
+                  "Converted/Altered Grasslands",
+                  "Forest", "Developed", "Water")
 
-cgr_border <- st_read("./data/GIS/Grasslands_Roadmap_boundary_Aug_2021.shp")
-cgr_border_wgs <- st_transform(cgr_border, 4326)
-st_write(cgr_border_wgs, "./data/GIS/Grasslands_Roadmap_boundary_Aug_2021_WGS84.shp", append = F)
+# add habitat legend to raster attribute table
+levels(cgr_ext_park)[[1]] <- dat
+#prod(res(cgr_ext_park)) #1278.99 m2
 
-# Load WGS 84 version of datasets
-nps_im_wgs <- st_read("./data/GIS/CGI_parks_network_wgs84.shp")
-nps_im_1km_wgs <- st_read("./data/GIS/CGI_parks_network_1km_wgs.shp")
-nps_im_10km_wgs <- st_read("./data/GIS/CGI_parks_network_10km_wgs.shp")
+# extract cells to values in a dataframe by nps unit
+park_ext <- raster::extract(cgr_ext_park, nps_im_val, na.rm = T, df = T, exact = T)
 
-# Try compressing raster for smaller size/faster plotting?
-# cgr_ras <- raster::raster("./data/GIS/CGR_GAM_V2_WGS84.tif")
-# raster::writeRaster(cgr_ras, filename = "./data/GIS/CGR_GAM_V2_WGS84_compress.tif", format = "GTiff",
-#                     options = c("COMPRESS=LZW"), datatype = "FLT4S", overwrite = T)
-cgr_ras <- raster::raster("./data/GIS/CGR_GAM_V2_WGS84_compress.tif")
+park_ext_df <- as.data.frame(park_ext)
 
-# I couldn't convert the raster CGR dataset to shapefile in R- too big.
-# Instead, I did this in ArcPro for the 10km clip, then am dissolving
-# to make smaller file, joining with nps units, then transforming to wgs84.
+# setting up full table values and legend for join
+cats <- data.frame(ID = c(0, 5, 7, 100, 500, 1000, 2000, 5000),
+                   Habitat = c("No Data", "Core Grassland", "Desert/Shrub",
+                               "Vulnerable Grasslands",
+                               "Converted/Altered Grasslands",
+                               "Forest", "Developed", "Water"))
 
-cgr_shp <- st_read("./data/GIS/CGR_GAM_V2_UTM_NAD83_10km.shp") #+++ REDO THIS IN ARC EVERY park update
-cgr_shp_diss <- sf_dissolve(cgr_shp, y = "gridcode")
-cgr_shp_park <- st_join(cgr_shp_diss, nps_im_10km)
-cgr_shp_park2 <- st_transform(cgr_shp_park, 4326)
-st_write(cgr_shp_park2, "./data/GIS/CGR_GAM_V2_10km_WGS84_diss.shp", append = F)
-#st_write(cgr_shp_park2, "./data/GIS/CGR_GAM_V2_10km_WGS84_park.shp", append = F)
+# join extracted cgr values with nps_im data
+nps_im_val$ID <- 1:nrow(nps_im_val)
+park_ext2 <- left_join(park_ext_df, nps_im_val, by = "ID") #+++++ Running here ++++++
+#+++++ ENDED HERE ON CODE UPDATE +++++
+head(park_ext2)
+
+# join with cgr values/habitat
+park_ext3 <- full_join(park_ext2, cats, by = c("Category" = "ID"))
+head(park_ext3)
+# sum number of cells in each habitat type by park
+park_ext4 <- park_ext3 |> group_by(ID, Habitat, UNIT_CODE, NETCODE) |>
+  summarize(num_cells = n(), .groups = 'drop') |>
+  filter(!is.na(ID))
+
+# make nps_im a dataframe and drop geometry
+nps_im_df <- as.data.frame(st_drop_geometry(nps_im_val))
+
+#park_ext5 <- left_join(park_ext4, nps_im_df[,c("UNIT_CO", "acres")], by = "UNIT_CODE") |> data.frame()
+
+# Use pivots to get all combinations of habitat types for each park
+park_ext6 <- park_ext5 |> dplyr::select(-ID) |>
+  pivot_wider(names_from = Habitat, values_from = num_cells, values_fill = 0) |>
+  pivot_longer(cols = 4:10, names_to = "Habitat", values_to = "num_cells")
+
+park_ext7 <- park_ext6 |> group_by(UNIT_CODE, NETCODE, acres) |>
+  mutate(total_cells = sum(num_cells, na.rm = T),
+         prop_hab = (num_cells/total_cells)*100,
+         acres_hab = (prop_hab/100) * acres,
+         prop_check = sum(prop_hab)) |>
+  arrange(UNIT_CODE, Habitat)
+
+write.csv(park_ext7, "./data/CGR_parks_prop_habitat.csv")
+
+#+++++ ENDED HERE ++++++ #+++ RUNNING HERE +++
+
+
 
 cgr_shp <- st_read("./data/GIS/CGR_GAM_V2_UTM_NAD83_10km.shp") |> st_transform(4326)
 st_write(cgr_shp, "./data/GIS/CGR_GAM_V2_WGS_10km.shp", append = F)
